82ea0b2239333cc14cf0a7b94f47df15cbe7021d
omar <omar24kamel2424@gmail.com>
2023-11-07 23:24:00 +0200

    Newly signed up doctors will redirected to a separate page

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -68,7 +68,7 @@ export const useSignup = () => {
                     dispatch({ type: 'LOGIN', payload: json })
 
                     setIsLoading(false)
-                    navigate('/doctor-home')
+                    navigate('/not-approved-doctor')
                 }
 
         }
--- frontend/src/pages/Doctor/DoctorHome.js
+++ frontend/src/pages/Doctor/DoctorHome.js
@@ -1,35 +1,35 @@
 
 import { Link } from "react-router-dom";
 
-const DoctorHome =() => {
-    
-  
-    return (
+const DoctorHome = () => {
 
-        <div className="admin-home" style={{ display: 'flex' }}>
-          <div className="home-button">
-            <Link to="/doctor-view-info">
-            <button>My Info</button>
-            </Link>
-          </div>
-          <div className="home-button">
-            <Link to="/doctor-view-appointments">
-            <button>My appointments</button>
-            </Link>
-          
-          </div>
-          <div className="home-button">
-            <Link to="/doctor-view-patients">
-            <button>My Patients</button>
-            </Link>
-          </div>
-          <div className="home-button">
-        <Link to="/doctor-change-password">
-        <button>Change Password</button>
+
+  return (
+
+    <div className="admin-home" style={{ display: 'flex' }}>
+      <div className="home-button">
+        <Link to="/doctor-view-info">
+          <button>My Info</button>
         </Link>
       </div>
-        </div>
-    
-      )
-    }
+      <div className="home-button">
+        <Link to="/doctor-view-appointments">
+          <button>My appointments</button>
+        </Link>
+
+      </div>
+      <div className="home-button">
+        <Link to="/doctor-view-patients">
+          <button>My Patients</button>
+        </Link>
+      </div>
+      <div className="home-button">
+        <Link to="/doctor-change-password">
+          <button>Change Password</button>
+        </Link>
+      </div>
+    </div>
+
+  )
+}
 export default DoctorHome;
